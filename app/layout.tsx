a7187979f95f0086cd4a822aeaaaade2f06d5a7d
oschmidek-collab <oschmidek@gmail.com>
2025-12-23 14:33:48 +0200

    add root layout

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "האם סינדי צודקת",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="he" dir="rtl">
+      <body>{children}</body>
+    </html>
+  );
+}
